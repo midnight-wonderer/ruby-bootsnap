732458f0d891dab2134758e3aa91ec80060a2fc2
Sarun Rattanasiri <midnight_w@gmx.tw>
2025-02-12 22:57:06 +0700

    test fork

--- lib/bootsnap/cli/worker_pool.rb
+++ lib/bootsnap/cli/worker_pool.rb
@@ -7,7 +7,7 @@ module Bootsnap
         def create(size:, jobs:)
           if size > 0 && Process.respond_to?(:fork)
             # Inline.new(jobs: jobs)
-            ThreadExecutor.new(size: size, jobs: jobs)
+            ForkExecutor.new(size: size, jobs: jobs)
             # new(size: size, jobs: jobs)
           else
             Inline.new(jobs: jobs)
@@ -50,116 +50,119 @@ module Bootsnap
         end
       end
 
-      class Worker
-        attr_reader :to_io, :pid
-
-        def initialize(jobs)
-          @jobs = jobs
-          @pipe_out, @to_io = IO.pipe(binmode: true)
-          # Set the writer encoding to binary since IO.pipe only sets it for the reader.
-          # https://github.com/rails/rails/issues/16514#issuecomment-52313290
-          @to_io.set_encoding(Encoding::BINARY)
-
-          @pid = nil
-        end
-
-        def write(message, block: true)
-          payload = Marshal.dump(message)
-          if block
-            to_io.write(payload)
-            true
-          else
-            to_io.write_nonblock(payload)
-          end
-        end
-
-        def close
-          to_io.close
-        end
-
-        def work_loop
-          loop do
-            job, *args = Marshal.load(@pipe_out)
-            return if job == :exit
-
-            @jobs.fetch(job).call(*args)
-          end
-        rescue IOError
-          nil
-        end
-
-        def spawn
-          @pid = Process.fork do
-            to_io.close
-            work_loop
-            exit!(0)
-          end
-          @pipe_out.close
-          true
-        end
-      end
-
-      def initialize(size:, jobs: [])
-        @size = size
-        @jobs = jobs
-        @queue = Queue.new
-        @pids = []
-      end
-
-      def spawn
-        @workers = @size.times.map { Worker.new }
-        @workers.each(&:spawn)
-        @dispatcher_thread = Thread.new { dispatch_loop }
-        @dispatcher_thread.abort_on_exception = true
-        true
-      end
-
-      def dispatch_loop
-        loop do
-          job = @queue.pop
-          $stderr.puts('jobjob')
-          return true unless job
-          begin
-            @workers.sample(random: ::SecureRandom).write(job, block: false)
-          rescue ::IO::WaitWritable
-            begin
-              free_worker.write(job, block: false)
-            rescue ::IO::WaitWritable
-              retry
+      class ForkExecutor
+        def initialize(size:, jobs: [])
+          @size = size
+          @jobs = jobs.to_a
+          @pids = []
+          @queue = ::Queue.new.tap do |q|
+            @jobs.each_with_index do |_job, index|
+              q.push(index)
             end
           end
-        rescue ::StandardError => e
-          $stderr.puts(['dbg', e.message, e.inspect])
-          raise "hohohohoh"
         end
-      ensure
-        @workers.each do |worker|
-          worker.write([:exit])
-          worker.close
-        end
-      end
 
-      def free_worker
-        @workers.map do |worker|
-          [worker.to_io, worker]
-        end.to_h.then do |mapping|
-          mapping[::IO.select(nil, mapping.keys)[1].sample(random: ::SecureRandom)]
+        def call
+          puts 'rr1'
+          @workers = @size.times.map do
+            Worker.new do |job_index|
+              @jobs[job_index]
+            end
+          end.tap do |workers|
+            workers.each(&:spawn)
+          end
+          @dispatcher_thread = Thread.new { dispatch_loop }
+          @dispatcher_thread.abort_on_exception = true
+          @queue.close
+          @dispatcher_thread.join
+          @workers.each do |worker|
+            _pid, status = Process.wait2(worker.pid)
+            return status.exitstatus unless status.success?
+          end
         end
-      end
 
-      def push(*args)
-        @queue.push(args)
-        nil
-      end
+        private
 
-      def shutdown
-        @queue.close
-        @dispatcher_thread.join
-        @workers.each do |worker|
-          _pid, status = Process.wait2(worker.pid)
-          return status.exitstatus unless status.success?
+        def dispatch_loop
+          puts 'rr2'
+          loop do
+            job = @queue.pop
+            return true unless job
+            begin
+              @workers.sample.write(job, block: false)
+            rescue ::IO::WaitWritable
+              begin
+                free_worker.write(job, block: false)
+              rescue ::IO::WaitWritable
+                retry
+              end
+            end
+          rescue ::StandardError => e
+            $stderr.puts(['dbg', e.message, e.inspect])
+            raise "hohohohoh"
+          end
+        ensure
+          @workers.each do |worker|
+            worker.write([:exit])
+            worker.close
+          end
         end
-        nil
+
+        def free_worker
+          @workers.map do |worker|
+            [worker.to_io, worker]
+          end.to_h.then do |mapping|
+            mapping[::IO.select(nil, mapping.keys)[1].sample(random: ::SecureRandom)]
+          end
+        end
+
+        class Worker
+          attr_reader :to_io, :pid
+
+          def initialize(&fetch_job)
+            @fetch_job = fetch_job
+            @pipe_out, @to_io = IO.pipe(binmode: true)
+            # Set the writer encoding to binary since IO.pipe only sets it for the reader.
+            # https://github.com/rails/rails/issues/16514#issuecomment-52313290
+            @to_io.set_encoding(Encoding::BINARY)
+
+            @pid = nil
+          end
+
+          def write(message, block: true)
+            payload = Marshal.dump(message)
+            if block
+              to_io.write(payload)
+              true
+            else
+              to_io.write_nonblock(payload)
+            end
+          end
+
+          def close
+            to_io.close
+          end
+
+          def work_loop
+            loop do
+              job_index, = Marshal.load(@pipe_out)
+              return if job_index == :exit
+              @fetch_job.call(job_index).call
+            end
+          rescue IOError
+            nil
+          end
+
+          def spawn
+            @pid = Process.fork do
+              to_io.close
+              work_loop
+              exit!(0)
+            end
+            @pipe_out.close
+            true
+          end
+        end
       end
     end
   end
